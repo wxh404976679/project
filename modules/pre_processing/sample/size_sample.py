027a3b4440f2cf11b95db4b13242d7d7a495a3e7
金国杨 <jinguoyang95@dingtalk.com>
2022-08-26 13:20:14 +0800

    extract data type util

--- modules/pre_processing/sample/size_sample.py
+++ modules/pre_processing/sample/size_sample.py
@@ -6,22 +6,23 @@ import sys
 
 import pandas as pd
 from pandas import DataFrame
-import math
+
+from modules.utils.data_type_util import convert_str_to_bool
+
 
 def size_sample(params: dict):
     input1_dir = params['params.input.1.dir']
     print("input1_dir", input1_dir)
-    df: DataFrame = pd.read_pickle(input1_dir)
-
     size: str = params['params.size']
     print("size", size)
     replace: str = params['params.withReplacement']
     print("withReplacement", replace)
-    res: DataFrame = df.sample(n=int(size), replace=replace.lower() == str(True).lower())
     output_dir = params['params.output.dir']
-    os.makedirs(output_dir, exist_ok=True)
     print("output_dir", output_dir)
 
+    df: DataFrame = pd.read_pickle(input1_dir)
+    res: DataFrame = df.sample(n=int(size), replace=convert_str_to_bool(replace))
+    os.makedirs(output_dir, exist_ok=True)
     df_write = pd.DataFrame(data=res, columns=res.columns)
     df_write.to_pickle(output_dir + "/1")
     # print(df_write)
---
+++ modules/utils/data_type_util.py
@@ -0,0 +1,2 @@
+def convert_str_to_bool(s: str):
+    return s.lower() in ['true', '1', 't', 'y', 'yes']
